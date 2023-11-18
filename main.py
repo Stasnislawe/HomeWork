73e4542501c4496bc902838e3f147a7a94fb1312
Stasnislawe <147979384+Stasnislawe@users.noreply.github.com>
2023-11-18 14:08:40 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,56 +1,16 @@
-maps = [1,2,3,4,5,6,7,8,9]
-winline = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+# This is a sample Python script.
 
-def print_maps():
-    print(maps[0], end=" ")
-    print(maps[1], end=" ")
-    print(maps[2])
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-    print(maps[3], end=" ")
-    print(maps[4], end=" ")
-    print(maps[5])
 
-    print(maps[6], end=" ")
-    print(maps[7], end=" ")
-    print(maps[8])
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-def step_maps(step, symbol):
-    ind = maps.index(step)
-    maps[ind] = symbol
 
-def result():
-    win = ""
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-    for i in winline:
-        if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
-            win = "X"
-        if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
-            win = "O"
-
-    return win
-
-game_over = False
-player1 = True
-
-while game_over == False:
-
-    print_maps()
-
-    if player1 == True:
-        symbol = "X"
-        step = int(input("Игрок 1, ваш ход: "))
-    else:
-        symbol = "O"
-        step = int(input("Игрок 2, ваш ход: "))
-
-    step_maps(step, symbol)
-    win = result()
-    if win != "":
-        game_over = True
-    else:
-        game_over = False
-
-    player1 = not (player1)
-
-print_maps()
-print("Победил игрок с символом:", win)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
