dc95f3d3b32fb1ec9f760f21a7fd6c235ce86acc
Stasnislawe <147979384+Stasnislawe@users.noreply.github.com>
2023-12-31 23:06:04 +0500

    Add files via upload

---
+++ NP2/simpleapp/views.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from django.views.generic import ListView, DetailView
+from .models import Product
+from .filters import ProductFilter
+
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
+    #paginate_by = 2
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = "Распродажа в среду!"
+        return context
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Product
+    # Используем другой шаблон — product.html
+    template_name = 'product.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'product'
